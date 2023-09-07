22fc84990c7c5350252a99dab64948008d18ef35
IgorMushk <igor.mushketik@gmail.com>
2023-09-07 23:24:33 +0300

    Add contacts.json to stats + App.styled + html App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template!
-    </div>
-  );
-};
+import { Component } from 'react';
+import contacts from '../data/contacts.json';
+import { Container } from './App.styled';
 
+export class App extends Component {
+  state = {
+    contactList: contacts,
+  };
 
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template!
-//     </div>
-//   );
-// };
+  //console.log('clientList', this.state.clientList);
+
+  render() {
+    console.log('clientList', this.state.contactList);
+    // return <Container>React homework template</Container>;
+    return <Container>
+      <h1>Phonebook</h1>
+      <div>Form</div>
+      <h2>Contacts</h2>
+      <div>Contact list</div>
+    </Container>;
+  }
+}
