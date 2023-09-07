c7ccd28afc4cca3426bc0f47342b064c7ea00259
IgorMushk <igor.mushketik@gmail.com>
2023-09-07 22:16:43 +0300

    install -  styled-componets + prop-types

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,3 +14,21 @@ export const App = () => {
     </div>
   );
 };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template!
+//     </div>
+//   );
+// };
