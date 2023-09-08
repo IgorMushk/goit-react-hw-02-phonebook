7e4049b3ecd658b26016823c8be85fc8ddb82328
IgorMushk <igor.mushketik@gmail.com>
2023-09-08 15:26:59 +0300

    Upd inputs from TZ

--- src/components/ContactForrm/ContactForm.jsx
+++ src/components/ContactForrm/ContactForm.jsx
@@ -1,26 +1,38 @@
-const { Component } = require("react");
+const { Component } = require('react');
 
 export class ContactForm extends Component {
-state = {
+  state = {
     name: '',
     number: '',
-}
-    
-render() {
-    return ( 
-    <>
-        <form >
-        <label >
-            Name
-        <input type="text" name="name" />
-        </label>
-        <label for="lname">
-            Number
-        <input type="tel" name="number" />
-        </label>
-        <button type="submit">Add Contact</button>
-      </form>   
-    </>)
-}
+  };
 
+  render() {
+    return (
+      <>
+        <form>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label for="lname">
+            Number
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button type="submit">Add Contact</button>
+        </form>
+      </>
+    );
+  }
 }
